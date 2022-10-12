123a185ec3ac71fc65ea6753daf2a3b34e4120a3
Arseni12345 <113444999+Arseni12345@users.noreply.github.com>
2022-10-12 14:54:56 +0300

    Update SubstringDataBase.java

--- src/main/java/com/example/test_tasks/domain/SubstringDataBase.java
+++ src/main/java/com/example/test_tasks/domain/SubstringDataBase.java
@@ -16,7 +16,7 @@ public class SubstringDataBase {
 
     @Id
     @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "substrings")
